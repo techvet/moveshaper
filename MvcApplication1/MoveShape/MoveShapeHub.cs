366505050be7d537338538f343124f2ac4dd62a6
U-Gabe-PC\Gabe <Gabe@Gabe-PC.(none)>
2014-05-14 00:54:57 -0500

    added second set of files

---
+++ MvcApplication1/MoveShape/MoveShapeHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace MvcApplication1.MoveShape
+{
+    [HubName("moveShapeHub")]
+    public class MoveShapeHub : Hub
+    {
+        public void MoveShape(int x, int y)
+        {
+            Clients.Others.shapeMoved(x, y);
+        }
+
+        public void MoveShape2(int x, int y)
+        {
+            Clients.Others.shapeMoved2(x, y);
+        }
+    }
+}
